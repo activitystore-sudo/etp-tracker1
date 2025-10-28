e8a250fac45ce0108e5e570819676a3f212dca9e
activitystore-sudo <activitystore@gmail.com>
2025-10-28 00:15:59 +0000

    Update password field validation in register schema
    Removed password length validation from schema.

--- endpoints/auth/register_with_password_POST.schema.ts
+++ endpoints/auth/register_with_password_POST.schema.ts
@@ -3,7 +3,7 @@ import { User } from "../../helpers/User";
 
 export const schema = z.object({
   email: z.string().email("Email is required"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
+  password: z.string(),
   displayName: z.string().min(1, "Name is required"),
 });
 
